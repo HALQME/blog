f09aaf301be960a5e42f9d1c901ffbd02a0fa95f
HAL <68320771+HALQME@users.noreply.github.com>
2026-04-07 17:37:34 +0900

    Simplify Nix flake configuration

--- flake.nix
+++ flake.nix
@@ -1,50 +1,31 @@
 {
-  description = "Marp environment";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
+  outputs = {nixpkgs, ...}: let
+    systems = ["x86_64-linux" "aarch64-darwin"];
+    for = f:
+      nixpkgs.lib.genAttrs systems (
+        system: f (import nixpkgs {inherit system;})
+      );
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        # Import nixpkgs for the current system (preferred over legacyPackages)
-        pkgs = import nixpkgs {inherit system;};
-
-        # Beautiful Japanese monospace fonts
-        fonts = [
-          pkgs.udev-gothic
-        ];
-
-        inputs = [pkgs.bun pkgs.typst fonts];
-
-        # Point to Nix store fonts
-        typst-font-paths = builtins.concatStringsSep ":" (map (font: "${font}/share/fonts") fonts);
-      in {
-        devShells = {
-          default = pkgs.mkShell {
-            buildInputs = inputs;
-
-            shellHook = ''
-              echo "📦 Available packages:"
-              echo "  - bun: $(bun --version)"
-              echo "  - typst: $(typst --version)"
-              # Nix store fonts
-              echo "🔧 TYPST_FONT_PATHS set to: ${typst-font-paths}"
-              export TYPST_FONT_PATHS="${typst-font-paths}"
-            '';
-
-            # Ensure we use a proper interactive shell
-            shellWrapper = pkgs.writeShellScript "dev-shell" ''
-              exec ${pkgs.zsh}/bin/zsh "$@"
-            '';
-          };
-        };
-      }
-    );
+    deps = pkgs:
+      with pkgs; [
+        bun
+        typst
+      ];
+    fonts = pkgs:
+      with pkgs; [
+        udev-gothic
+      ];
+  in {
+    devShells = for (pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = deps pkgs;
+        shellHook = ''
+          export TYPST_FONT_PATHS="${builtins.concatStringsSep ":" (map (font: "${font}/share/fonts") (fonts pkgs))}"
+        '';
+      };
+    });
+  };
 }
